5bddc8887eed2e39835614c38d0dbe0c5972a479
David <david.cabello.info@gmail.com>
2021-11-30 14:54:21 +0100

    first commit

---
+++ src/main/java/com/dscode/aplication/MicroservicesPrometheusApplication.java
@@ -0,0 +1,13 @@
+package com.dscode.aplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesPrometheusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesPrometheusApplication.class, args);
+	}
+
+}
